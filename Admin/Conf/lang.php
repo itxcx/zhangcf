1ee1ce813776ac940ad4e167740e5c9a9710a90e
王全发 <wqf5359@163.com>
2016-02-22 15:05:06 +0800

    多语言设置开关及部分语种管理

---
+++ Admin/Conf/lang.php
@@ -0,0 +1,15 @@
+<?php
+return array (
+  'LANG' => 
+  array (
+    'USE' => true,
+    'SET' => 
+    array (
+      0 => 'zh-cn',
+      1 => 'en-us',
+      2 => 'zh-tw',
+    ),
+    'DEFAULT' => 'zh-cn',
+  ),
+);
+?>

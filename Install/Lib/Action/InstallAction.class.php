634e3626f252e471a69984d63329ae16c09c89cb
王全发 <wqf5359@163.com>
2016-04-06 16:02:19 +0800

    md100加密方式修改，及添加安装程序检测php版本

--- Install/Lib/Action/InstallAction.class.php
+++ Install/Lib/Action/InstallAction.class.php
@@ -10,6 +10,7 @@ class InstallAction extends Action
 
 	function _initialize()
     {
+		if(version_compare(PHP_VERSION,'5.3.0','<'))  throw_exception('当前php版本小于5.3！请使用5.3及以上版本');
 		if((isset($_REQUEST['step']) ? $_REQUEST['step']:'') !='done' && ACTION_NAME != 'done' && file_exists('./install.lock')){
 			die('重新安装请删除/Install/install.lock文件');
 		}
@@ -294,6 +295,8 @@ class InstallAction extends Action
 
 		if($super_admin=='') exit( '请输入管理员帐号' );
 
+		//生成随机密码文件
+		$this->rand_pass();
 
 		//创建核心配置文件
 		$result	= $Install->create_core_config($db_host, $db_port, $db_user, $db_pass, $db_name, $timezone, $super_admin, $this->langs);
@@ -444,16 +447,6 @@ class InstallAction extends Action
 			if(!in_array($clientip,$passed_ips)){
 				file_put_contents(ROOT_PATH.'Install/install.lock','重新安装请删除该文件!');
 			}
-			!is_dir(ROOT_PATH.'ThinkPHP/config') && mkdir(ROOT_PATH.'ThinkPHP/config',0777,true);
-			$dh = opendir(ROOT_PATH.'ThinkPHP/config');
-			while ($file=readdir($dh)) {
-				if($file!='.' && $file!='..') {
-					$fullpath=ROOT_PATH.'ThinkPHP/config/'.$file;
-					if(!is_dir($fullpath)) {
-						unlink($fullpath);
-					}
-				}
-			}
 			$t = true;
 			B('SaveConfig',$t);
 			
@@ -467,6 +460,33 @@ class InstallAction extends Action
 	public function done()
 	{
 		$this->display();
+	}
+	
+	/*
+	* 生成随机密码文件
+	*/
+	public function rand_pass()
+	{
+		!is_dir(THINK_PATH .'config') && mkdir(THINK_PATH .'config',0777,true);
+		$dh = opendir(THINK_PATH.'config');
+		while ($file=readdir($dh)) {
+			if($file != '.' && $file != '..' && $file != 'password.php') {
+				$fullpath = THINK_PATH .'config/'.$file;
+				if(!is_dir($fullpath)) {
+					unlink($fullpath);
+				}
+			}
+		}
+		if(!file_exists(THINK_PATH .'config/password.php')){ 
+			$rand = '';
+			$str = '0123456789abcdefghijklmnopkrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			$length = strlen($str);
+			for($i=0;$i<512;$i++){
+				$start = mt_rand(0,$length-1);
+				$rand .= substr($str,$start,1);
+			}
+			file_put_contents(THINK_PATH .'config/password.php',"<?php\nreturn '" . $rand . "';\n?>");
+		}
 	}
 }
 ?>

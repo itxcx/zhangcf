7f796412bb894b18a46248cb67a129120f9c5a06
王全发 <wqf5359@163.com>
2016-04-11 12:13:14 +0800

    支付返回提示页面

--- Admin/Lib/Action/PaymentAction.class.php
+++ Admin/Lib/Action/PaymentAction.class.php
@@ -12,30 +12,46 @@ class PaymentAction extends Action{
 	
 	//处理支付运营商返回信息
 	public function receive(){
+	/*	$_REQUEST= array(
+		  "MerOrderNo" =>"20160329105100382609",
+		  "Currency" =>"CNY",
+		  "MerCode" =>"10125801",
+		  "Amount" =>"0.10",
+		  "OrderDate" =>"20160329",	  
+		  "Succ" =>"Y",
+		  "Msg" =>"success",
+		  "GoodsInfo" =>"",
+		  "SysOrderNo" =>"BO160329105101511cd23",
+		  "RetencodeType" =>"12",
+		  "Signature" =>"e3b8cd1b553c2cdfbadc41ea58d0d79b"
+         );*/
 		if(I("request./a")){
 			M()->startTrans();
 			//记录返回的数据
-           	F('paytest',I("request./a"));
+           	F('paytest',$_REQUEST);
            	//获取支付接口的信息
 			$lista=F('interface_data');
 			//获取已安装的接口的直联银行信息
 			//$listb=F('banklist');
-			//
+	
 			$listc=$lista;//array_intersect_key ($lista,$listb);		//获取可能产生支付订单的列表（包含支付接口的订单KEY）
 			//循环获得支付的订单单号
+
 			foreach($listc as $key=>$value){
 				//如果找到了订单号  则跳出执行回调处理订单状态
+				
 				if(I("request.".$value['order_key']."/s")){
-					$where['orderId'] = I("request.".$value['order_key']."/s");
+				
+					$where['orderId']=I("request.".$value['order_key']."/s");
 					import("Admin.Pay.Pay");
 					//根据订单号找到要处理的订单
 					$PayOrder= M('PayOrder');
-					$info=$PayOrder->where($where)->find();		//根据订单号查询订单库					
+					$info=$PayOrder->where($where)->find();		//根据订单号查询订单库
+
 					if(!empty($info)){
-						//$arr['info'] = $info;
 						$payment= $info['payment_class'];		//获取支付接口名
 						$pay=new Pay($payment,false);			//这里交给核心类处理
-						$result = $pay->receive($where['orderId']);	//支付接口判断支付成功还是失败
+						$result = $pay->receive($where['orderId']);				//支付接口判断支付成功还是失败
 					}
 					break;
 				}
